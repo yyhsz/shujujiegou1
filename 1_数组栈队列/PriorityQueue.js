ecbbf1e709e0dcfa4aa40a4a44aabdbbad8fc2b3
余银海 <591985864@qq.com>
2021-02-13 18:35:32 +0800

    优先级队列

--- 1_数组栈队列/PriorityQueue.js
+++ 1_数组栈队列/PriorityQueue.js
@@ -1,28 +1,33 @@
+// 一般 以表示优先级的数字越小 优先级越高
+// 需要修改入队操作
 class QueueElement {
   constructor(ele, priority) {
     this.ele = ele
     this.priority = priority
   }
 }
-class PriorityQueue {
+class PriorityQueue extends Queue {
   constructor() {
+    super()
     this.items = []
   }
   enqueue(ele, priority) {
-    new QueueElement(ele, priority)
+    const qEle = new QueueElement(element, priority)
+
     let added = false
     if (this.items.length === 0) {
-      this.items.push(new QueueElement(ele, priority))
+      this.items.push(qEle)
     } else {
       for (let i = 0; i < this.items.length - 1; i++) {
+        // 最前面的优先级最高，数字最小
         if (this.items[i].priority > priority) {
-          this.items.splice(i, 0, new QueueElement(ele, priority))
+          this.items.splice(i, 0, qEle)
           added = true
           break
         }
       }
-      !added && this.items.push(new QueueElement(ele, priority))
+      !added && this.items.push(qEle)
     }
   }
 }
-module.exports = { PriorityQueue }
+// module.exports = { PriorityQueue }
